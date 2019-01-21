3bc71767e9d6d81d6396546b9482c8f699ab6830
Demilad <demileye06@yahoo.co.uk>
2019-01-21 12:07:39 +0100

    Improved Javascript

--- js/imageLoad.js
+++ js/imageLoad.js
@@ -1,6 +1,6 @@
 function loadJSON(callback) {   
 
-    var xobj = new XMLHttpRequest();
+    const xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
     xobj.open('GET', 'js/logos.json', true);
     xobj.onreadystatechange = function () {
@@ -12,21 +12,19 @@ function loadJSON(callback) {
  }
 
  function init() {
-    loadJSON(function(response) {
-       var actual_JSON = JSON.parse(response);
-       createLogos(actual_JSON);
-       
+    loadJSON(response => {
+       const logoArray = JSON.parse(response);
+       createLogos(logoArray);
     });
    }
 
 init();
 
-function createLogos(json) {
-        for (const logo of json) {
+function createLogos(logoArray) {
+        logoArray.forEach(logo => {
             const html = `<div class='logo'> <div class='logo__holder'> <div class='logo__image'> <img src='logos/${logo.filename}/${logo.filename}.svg' alt='${logo.title}logo'> </div><div class='logo__download'> <div class='logo__download__overlay'> <a href='logos/${logo.filename}/${logo.filename}.svg' download='${logo.title}Logo'> <span class='logo__download__overlay--svg'> Download SVG </span> </a> <a href='logos/${logo.filename}/${logo.filename}.png' download='${logo.title}Logo'> <span class='logo__download__overlay--png'> Download PNG </span> </a> </div></div></div><div class='logo__text'> <p class='logo__text--primary'>${logo.title}</p><p class='logo__text--secondary'>${logo.category}</p></div></div>`;
             
             const main = document.querySelector('main');
             main.insertAdjacentHTML("beforeend", html);
-        }
-        
+        });      
 }
