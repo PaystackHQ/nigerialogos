12da20678a3cc6adfdab2ad411f1e616cfc35aac
Demilad <demileye06@yahoo.co.uk>
2019-01-21 12:15:59 +0100

    Consistent single quotes

--- js/imageLoad.js
+++ js/imageLoad.js
@@ -25,6 +25,6 @@ function createLogos(logoArray) {
             const html = `<div class='logo'> <div class='logo__holder'> <div class='logo__image'> <img src='logos/${logo.filename}/${logo.filename}.svg' alt='${logo.title}logo'> </div><div class='logo__download'> <div class='logo__download__overlay'> <a href='logos/${logo.filename}/${logo.filename}.svg' download='${logo.title}Logo'> <span class='logo__download__overlay--svg'> Download SVG </span> </a> <a href='logos/${logo.filename}/${logo.filename}.png' download='${logo.title}Logo'> <span class='logo__download__overlay--png'> Download PNG </span> </a> </div></div></div><div class='logo__text'> <p class='logo__text--primary'>${logo.title}</p><p class='logo__text--secondary'>${logo.category}</p></div></div>`;
             
             const main = document.querySelector('main');
-            main.insertAdjacentHTML("beforeend", html);
+            main.insertAdjacentHTML('beforeend', html);
         });      
 }
